1bf20527258f927ea5ab5330f79d066470c4bd63
Shriyansh Mishra <shriyansh925@gmail.com>
2025-09-05 17:21:53 +0530

    build error maybe fixed :)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home.js';
+import Home from './components/Home';
 import QuizPage from './components/QuizPage';
 import ResultsPage from './components/ResultsPage';
 import NavBar from './components/NavBar';
