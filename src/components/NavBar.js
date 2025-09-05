f7f47a704cb35058b5173f90a67b345455b9a56e
Shriyansh Mishra <shriyansh925@gmail.com>
2025-09-05 16:45:49 +0530

    added a new navbar to navigate and configured

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+const NavBar = () => {
+  const location = useLocation();
+  const isHome = location.pathname === '/';
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-inner">
+        <Link to="/" className="navbar-brand">🧠 Quiz Master</Link>
+        <div className="navbar-actions">
+          {!isHome && (
+            <Link to="/" className="btn btn-secondary">Home</Link>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
+
